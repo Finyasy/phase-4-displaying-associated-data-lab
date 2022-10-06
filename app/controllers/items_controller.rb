bee0f2f3d2421ef97b54c3b3c2f20e41e43d4679
Bryan Bosire <bryanbosire@gmail.com>
2022-10-06 08:03:01 +0300

    Done.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+    # GET /items
+    # returns an array of all items with user info
+    def index
+        items = Item.all
+        render json: items, include: :user
+    end
 end
